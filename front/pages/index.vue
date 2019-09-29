40d56840241b203742d86fde5259b7e20b95477a
鷹取敏志 <takatorix@gmail.com>
2019-09-29 22:42:36 +0900

    add more

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,49 +1,71 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="block">
-        <div class="block video-block" v-for="item in items">
-          <AppVideo
-            :item="item"
-            :video-id="item.id"
-          />
+    <section class="section">
+        <div class="container">
+            <div class="block">
+                <div class="block video-block" v-for="item in items">
+                    <AppVideo
+                            :item="item"
+                            :video-id="item.id"
+                    />
+                </div>
+            </div>
+
+            <div class="block">
+                <nav class="pagination">
+                    <a href.prevent="#" class="pagination-next" @click="loadMore">More</a>
+                </nav>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
-  import ROUTES from '~/routes/api'
-  import AppVideo from '~/components/AppVideo'
+    import ROUTES from '~/routes/api'
+    import AppVideo from '~/components/AppVideo'
 
-  export default {
+    export default {
 
-    components: {AppVideo},
+        components: {AppVideo},
 
-    computed: {
-      items() {
-        return this.$store.getters.getPopularVideos
-      }
-    },
+        computed: {
+            items() {
+                return this.$store.getters.getPopularVideos
+            },
+            nextPageToken() {
+                return this.$store.getters.getMeta.nextPageToken
+            },
+        },
 
-    // ページがレンダリングされる前に
-    // データをストアに設定するために使用する
-    // contextオブジェクトを引数として受け取る
-    async fetch({store}) {
-      const payload = {
-        uri: ROUTES.GET.POPULARS
-      };
-      if (store.getters.getPopularVideos && store.getters.getPopularVideos.length > 0) {
-        return
-      }
-      await store.dispatch('fetchPopularVideos', payload)
+        methods: {
+            loadMore() {
+                const payload = {
+                    uri: ROUTES.GET.POPULARS,
+                    params: {
+                        pageToken: this.nextPageToken
+                    }
+                };
+                this.$store.dispatch('fetchPopularVideos', payload)
+            }
+        },
+
+
+        // ページがレンダリングされる前に
+        // データをストアに設定するために使用する
+        // contextオブジェクトを引数として受け取る
+        async fetch({store}) {
+            const payload = {
+                uri: ROUTES.GET.POPULARS
+            };
+            if (store.getters.getPopularVideos && store.getters.getPopularVideos.length > 0) {
+                return
+            }
+            await store.dispatch('fetchPopularVideos', payload)
+        }
     }
-  }
 </script>
 
 <style scoped>
-  .video-block {
-    max-width: 900px;
-  }
+    .video-block {
+        max-width: 900px;
+    }
 </style>
--- front/store/index.js
+++ front/store/index.js
@@ -1,27 +1,30 @@
 import {createRequestClient} from "./request-client";
 
 export const state = () => ({
-  items: [],
-  meta: {},
+    items: [],
+    meta: {},
 })
 
 export const actions = {
-  async fetchPopularVideos({commit}, payload) {
-    const client = createRequestClient(this.$axios)
-    const res = await client.get(payload.uri, payload.params)
-    commit('mutatePopularVideos', res)
-  }
+    async fetchPopularVideos({commit}, payload) {
+        const client = createRequestClient(this.$axios)
+        const res = await client.get(payload.uri, payload.params)
+        commit('mutatePopularVideos', res)
+    }
 }
 
 export const mutations = {
-  mutatePopularVideos(state, payload) {
-    state.items = payload.items ? state.items.concat(payload.items) : []
-    state.meta = payload
-  },
+    mutatePopularVideos(state, payload) {
+        state.items = payload.items ? state.items.concat(payload.items) : []
+        state.meta = payload
+    },
 }
 
 export const getters = {
-  getPopularVideos(state) {
-    return state.item
-  },
+    getPopularVideos(state) {
+        return state.item
+    },
+    getMeta(state) {
+        return state.meta
+    },
 }
